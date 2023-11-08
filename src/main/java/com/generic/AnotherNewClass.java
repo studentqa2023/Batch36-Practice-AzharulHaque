56785ec7c594cabe1aa89d3b472453655ae25fe5
smarttech#2023 <rezwanislam857@gmail.com>
2023-11-08 12:02:18 -0500

    Added AnotherNewClass in com.generic package

---
+++ src/main/java/com/generic/AnotherNewClass.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class AnotherNewClass {
+	
+	public static void newMethod() {
+		System.out.println("This is wnother new class.");
+	}
+	
+	public static void main(String[] args) {
+		newMethod();
+	}
+
+}
